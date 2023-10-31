41524d4c7744535deb6d26113bb56e2a0091e523
ION-dgu <93267274+ION-dgu@users.noreply.github.com>
2023-10-31 15:57:07 +0900

    Update cow.sh

--- scripts/EXP-LongForecasting/Linear/cow.sh
+++ scripts/EXP-LongForecasting/Linear/cow.sh
@@ -38,32 +38,3 @@ python -u run_longExp.py \
   # --des 'Exp' \
   # --itr 1 --batch_size 1024 --learning_rate 0.0005 >logs/LongForecasting/$model_name'_'cow_$seq_len'_'pred_6_threshold5_notrain.log
 
-# python -u run_longExp.py \
-#   --is_training 1 \
-#   --root_path ./dataset/ \
-#   --data_path cow.csv \
-#   --model_id cow_$seq_len'_'threshold_0.6_2 \
-#   --model $model_name \
-#   --data custom \
-#   --features MS \
-#   --seq_len $seq_len \
-#   --label_len 6 \
-#   --pred_len 6 \
-#   --enc_in 8 \
-#   --des 'Exp' \
-#   --itr 3 --batch_size 2048  --learning_rate 0.0005 >logs/LongForecasting/$model_name'_'cow_$seq_len'_'pred_6_threshold5_2.log 
-
-# python -u run_longExp.py \
-#   --is_training 1 \
-#   --root_path ./dataset/ \
-#   --data_path cow.csv \
-#   --model_id cow_$seq_len'_'threshold_0.6_3 \
-#   --model $model_name \
-#   --data custom \
-#   --features MS \
-#   --seq_len $seq_len \
-#   --label_len 6 \
-#   --pred_len 6 \
-#   --enc_in 8 \
-#   --des 'Exp' \
-#   --itr 5 --batch_size 512 --learning_rate 0.0005 >logs/LongForecasting/$model_name'_'cow_$seq_len'_'pred_6_threshold5_3.log 
